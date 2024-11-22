beb2a24512dfa02acd2637d26f5dd3b381f0816c
jsdlamini <johnsjdsd@gmail.com>
2024-11-22 13:00:03 +0200

    Folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup 
+
+setup(
+    name = "Generative Medical AI",
+    version="0.0.0",
+    author="J.S. Dlamini",
+    author_email="johnsjdsd@gmail.com",
+    packages= find_packages(),
+    install_requires = []
+)
